4a2be394518e66089a3b7e8fb47fc2e8662fd766
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-04-02 19:46:02 +0100

    build Sphere method

--- ColorSpheres/Sphere.cs
+++ ColorSpheres/Sphere.cs
@@ -4,9 +4,33 @@ namespace ColorSpheres
 {
     public class Sphere
     {
-        private static void Main(string[] args)
+        private byte _color;
+        private float _radius;
+        private int _throwCount;
+
+        public Sphere(byte color, float radius)
         {
-            Console.WriteLine("Hello LP1!");
+            _color = color;
+            _radius = radius;
+            _throwCount = 0;
+        }
+
+        public void Pop()
+        {
+            radius = 0;
+        }
+
+        public void Throw()
+        {
+            if (_radius > 0)
+            {
+                _throwCount++;
+            }
+        }
+
+        public int GetTimesThrown()
+        {
+            return _throwCount;
         }
     }
 }
