115baa9af652b81f3a7b25fb9b8ae5bf9a25a4cb
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-04-04 17:39:08 +0100

    First attempt

--- MyRoguelike/Hero.cs
+++ MyRoguelike/Hero.cs
@@ -7,7 +7,48 @@ namespace MyRoguelike
 {
     public class Hero
     {
-        private int _xp;
-        private float _health;
+        private int xp;
+        private float health;
+        public Hero (string name)
+        {
+            Name = name;
+            health = MaxHealth;
+            xp = 0;
+        }
+        public string Name { get; private set; }
+        public int Level => 1 + xp / 1000;
+        public int XP
+        {
+            get => xp;
+            set
+            {
+            if (value > xp)
+                {
+                    xp = value;
+                }
+            }
+            
+        }        
+        public float MaxHealth => 100 + (Level -1) * 20;
+        public float Health()
+        {
+            if (health < 0)
+            {
+                return health = 0;
+            }
+            if (health > MaxHealth)
+            {
+                return health = MaxHealth;
+            }
+            else
+            {
+                return health;
+            }
+        }
+        public void TakeDamage(float damage)
+        {
+            health -= damage;
+            xp += (int)(damage / 20);
+        }
     }
 }
